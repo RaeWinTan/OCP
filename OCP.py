a40756f0851c993833b3bf9ddc9bafa11cbbf9c5
thomaspeyrin <49208090+thomaspeyrin@users.noreply.github.com>
2026-01-19 10:42:50 +0800

    update get_var_model

--- OCP.py
+++ OCP.py
@@ -129,4 +129,12 @@ if __name__ == "__main__":
     test_diff_attack_milp(cipher)
     test_diff_attack_sat(cipher)
     # cipher = speck.SPECK_BLOCKCIPHER(r=6, version=[32,64])
-    """
+"""
+    import primitives.simon as simon
+    cipher = simon.SIMON_BLOCKCIPHER(r=12, version=[32,64])
+
+    test_all_implementations(cipher)
+    cipher.add_copy_operators()
+    test_visualisation(cipher)
+    test_diff_attack_milp(cipher)
+    #test_diff_attack_sat(cipher)
